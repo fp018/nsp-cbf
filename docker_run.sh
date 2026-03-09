7f0087dc51ef5a3fbe3dae6265fd14edd6738ce7
fp018 <pagano.francesca2@gmail.com>
2026-03-10 00:24:55 +0100

    gripper package

--- docker_run.sh
+++ docker_run.sh
@@ -7,6 +7,8 @@ docker run -it --rm --privileged \
 --env=LOCAL_USER_ID="$(id -u)" \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v $(pwd)/iiwa_qp:/catkin_ws/src/iiwa_qp:rw \
+-v $(pwd)/weiss_wsg50:/catkin_ws/src/weiss_wsg50:rw \
+-v $(pwd)/kuka_iiwa_support:/catkin_ws/src/kuka_iiwa_support:rw \
 -v $(pwd)/launch.yml:/catkin_ws/src/launch.yml:rw \
 --env="DISPLAY=$DISPLAY" \
 --network host \
